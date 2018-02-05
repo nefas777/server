f3b33018736cef6cd4ca54b20f3801f1fd6a91ea
nefas777 <36028717+nefas777@users.noreply.github.com>
2018-02-05 11:20:42 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/gunicorn.conf.py
-sudo /etc/init.d/gunicorn restart
+sudo gunicorn -b 0.0.0.0:8080 hello:app

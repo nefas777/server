f8156053853ad083f3000429c304847f54504150
nefas777 <36028717+nefas777@users.noreply.github.com>
2018-02-05 10:34:43 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ def app(environ, start_response):
   status = '200 OK'
   headers = [("Content-Type", "text/plain")]
   print(environ['QUERY_STRING'])
-  d = '\n'.join(environ['QUERY_STRING'].split('&'))
+  d = bytes('\n', 'ascii').join(environ['QUERY_STRING'].split('&'))
   print(d)
   start_response(status, headers)
   return [d]

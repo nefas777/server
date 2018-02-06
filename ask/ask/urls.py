55a1d94604d86365c01ac19c4cdb95aee18311b8
nefas777 <36028717+nefas777@users.noreply.github.com>
2018-02-06 22:36:34 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -17,11 +17,11 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-   url(r'^.*$', 'qa.views.test'),                                                              
-   url(r'^login/.*$', 'qa.views.test'),                                    
-   url(r'^signup/.*', 'qa.views.test'),                                   
+   url(r'^$', 'qa.views.test'),                                                              
+   url(r'^login/$', 'qa.views.test'),                                    
+   url(r'^signup/', 'qa.views.test'),                                   
    url(r'^question/\d+/$', 'qa.views.test'),                 
-   url(r'^ask/.*', 'qa.views.test'),                                         
-   url(r'^popular/.*', 'qa.views.test'),                                 
-   url(r'^new/.*', 'qa.views.test'),                                         
+   url(r'^ask/', 'qa.views.test'),                                         
+   url(r'^popular/', 'qa.views.test'),                                 
+   url(r'^new/', 'qa.views.test'),                                         
 ]

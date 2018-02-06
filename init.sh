e7443466aaadc81bdb778743809400087f1038e7
nefas777 <36028717+nefas777@users.noreply.github.com>
2018-02-06 10:19:51 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo ln -s /home/box/web/etc/gunicorn-django.conf   /etc/gunicorn-django.d/test1
+sudo ln -s /home/box/web/etc/gunicorn-django.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart

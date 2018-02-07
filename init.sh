e53f12e031e15a47c21a4788939793fc9d864153
nefas777 <36028717+nefas777@users.noreply.github.com>
2018-02-07 19:59:34 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn_django.conf.py   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+#sudo ln -s /home/box/web/etc/gunicorn_django.conf.py   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
+sudo gunicorn --bind 0.0.0.0:8000 --access-logfile acc.log --error-logfile err.log ask.wsgi:application

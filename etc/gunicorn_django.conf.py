79265094c8310c54ae04bf8676f1a5f1f97d48d4
nefas777 <36028717+nefas777@users.noreply.github.com>
2018-02-07 16:22:51 +0300

    Update gunicorn_django.conf.py

--- etc/gunicorn_django.conf.py
+++ etc/gunicorn_django.conf.py
@@ -1,7 +1,8 @@
 print('In Gunicorn config')
 CONFIG = { 
   'mode': 'wsgi',
-  'working_dir': '/home/box/web/ask',
+  'working_dir': '/home/box/web',
+  'pythonpath' : '/home/box/web/ask',
   'args': (
       '--bind=0.0.0.0:8000',
       'wsgi:application'

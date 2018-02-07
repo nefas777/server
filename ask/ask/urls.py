89be21cabb48a46d638348c1660990fb62a91ad3
nefas777 <36028717+nefas777@users.noreply.github.com>
2018-02-07 20:39:48 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,23 +1,23 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import *
 
 from django.contrib import admin
 admin.autodiscover()
 
-#urlpatterns = [
-#    url(r'', include('qa.urls')),
-#    url(r'^login/', include('qa.urls')),
-#    url(r'^signup/', include('qa.urls')),
-#    url(r'^question/(\d+)', include('qa.urls')),
-#    url(r'^ask/.*$', include('qa.urls')),
-#    url(r'^popular/', include('qa.urls')),
-#    url(r'^new/', include('qa.urls')),
-#]
 urlpatterns = [
-    url(r'^$', 'qa.views.test'),
-    url(r'^login/.*$', 'qa.views.test'),
-    url(r'^signup/.*', 'qa.views.test'),
-    url(r'^question/\d+/$', 'qa.views.test'),
-    url(r'^ask/.*$', 'qa.views.test'),
-    url(r'^popular/.*$', 'qa.views.test'),
-    url(r'^new/.*$', 'qa.views.test'),
+    url(r'', include('qa.urls')),
+    url(r'^login/', include('qa.urls')),
+    url(r'^signup/', include('qa.urls')),
+    url(r'^question/(\d+)', include('qa.urls')),
+    url(r'^ask/.*$', include('qa.urls')),
+    url(r'^popular/', include('qa.urls')),
+    url(r'^new/', include('qa.urls')),
 ]
+#urlpatterns = [
+#    url(r'^$', 'qa.views.test'),
+#    url(r'^login/.*$', 'qa.views.test'),
+#    url(r'^signup/.*', 'qa.views.test'),
+#    url(r'^question/\d+/$', 'qa.views.test'),
+#    url(r'^ask/.*$', 'qa.views.test'),
+#    url(r'^popular/.*$', 'qa.views.test'),
+#    url(r'^new/.*$', 'qa.views.test'),
+#]
